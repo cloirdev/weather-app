ada8317f9576aec028416ec9b6f407a5f4d72ea6
CloirDev <cloir.dev@gmail.com>
2025-09-25 21:10:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const form = document.getElementById("locationForm");
+const input = document.getElementById("locationInput");
+const weatherData = document.getElementById("weatherData");
+const loadingMessage = document.getElementById("loadingMessage");
+
+async function getWeather(location) {
+  loadingMessage.style.display = "block";
+  weatherData.innerHTML = "";
+
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=9BHRYLKGR7M29QPLN6UWSXHM6&contentType=json`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error al buscar la ubicación: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const temp = data.currentConditions.temp;
+    const conditions = data.currentConditions.conditions;
+    const iconName = data.currentConditions.icon;
+
+    const iconUrl = `imagenes/${iconName}.png`;
+
+    weatherData.innerHTML = `
+            <h2>${data.resolvedAddress}</h2>
+            <img src="${iconUrl}" alt="${conditions}">
+            <p>Temperatura: ${temp}°C</p>
+            <p>Condiciones: ${conditions}</p>
+        `;
+  } catch (error) {
+    weatherData.textContent = `Error: No se pudo obtener el clima. Por favor, revisa la ubicación.`;
+    console.error(error);
+  } finally {
+    loadingMessage.style.display = "none";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = input.value;
+  getWeather(location);
+});
